f5e685bde0bd4b5d52dd5413861a208859c1a384
Daniel Martin <danielmartin1205@gmail.com>
2020-01-21 19:58:34 -0500

    added mvp

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const jwt = require("jsonwebtoken");
+
+const bcrypt = require("bcryptjs");
+
+const db = require("./dbHelpers/users");
+
+const secrets = require("./Secrets/index");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+const verifyToken = async (req, res, next) => {    
+    try{
+        const token = req.headers.authorization;
+        const decoded = jwt.verify(token, secrets.jwt);
+      next();
+    } catch(err) {
+        next(err);
+    }
+}
+
+const generateToken = (user) => {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+    };
+
+    const options = {
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+
+server.post("/api/register", async (req, res, next) => {
+    try {
+        req.body.password = bcrypt.hashSync(req.body.password, 12);
+        res.status(201).json(await db.add(req.body));
+    } catch(err) {
+        next(err);
+    }
+});
+
+server.post("/api/login", async(req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await db.findBy({ username }).first();
+
+        const passwordValid = bcrypt.compareSync(password, user.password);
+
+        if (user && passwordValid) {
+            const token = generateToken(user);
+            res.status(200).json({ message: "Logged in", token })
+        } else {
+            res.status(401).json({ message: "Invalid Credentials" });
+        }
+    } catch(err) {
+        next(err);
+    }
+});
+
+server.get("/api/users", verifyToken, async (req, res, next) => {
+    try {
+        if (req.decoded) {
+            res.json(await db.find());   
+        } else {
+            console.log("no token")
+        }
+
+    } catch(err) {
+        next(err);
+    }
+});
+
+
+server.use((err, req, res, next) => {
+    console.log(err);
+
+    req.status(500).json({message: "Something went wrong"});
+});
+
+server.listen(PORT, () => {
+    console.log(`\n *** Server running on port:${PORT} *** \n`);
+});
